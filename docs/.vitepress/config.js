faf1149ca1ee0e8c322b3b413528d509686dff53
huangyul <1103221645@qq.com>
2024-11-15 10:11:29 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,21 @@
+export default {
+  base: "/my-blog/",
+  title: "测试title",
+  description: "blog",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide/" },
+      { text: "External", link: "https://google.com" }
+    ],
+    sidebar: [
+      {
+        text: "Bar",
+        items: [
+          { text: "Foo", link: "/foo/" },
+          { text: "Bar", link: "/bar/" }
+        ]
+      }
+    ],
+  }
+}
